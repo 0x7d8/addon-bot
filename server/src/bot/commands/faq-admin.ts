fad3c6556d3f518309b022b706e3adbe968de322
Anthony01M <anthony19990maalouf@gmail.com>
2024-10-10 13:15:37 +0000

    feat: faq-admin

---
+++ server/src/bot/commands/faq-admin.ts
@@ -0,0 +1,72 @@
+import Command from "@/bot/command"
+import { PermissionFlagsBits } from "discord.js"
+import { eq, ilike } from "drizzle-orm"
+import addFaqModal from "@/bot/modals/faq/add-faq" 
+
+export default new Command()
+	.build((builder) => builder
+		.setName('faq-admin')
+		.setDMPermission(false)
+		.setDescription('Administrator commands - Add or remove faqs')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addSubcommand((subcommand) => subcommand
+            .setName('add')
+            .setDescription('Add a new FAQ')
+        )
+        .addSubcommand((subcommand) => subcommand
+            .setName('remove')
+            .setDescription('Remove a FAQ')
+            .addIntegerOption((option) => option
+                .setName('faq')
+                .setDescription('The faq you want to remove')
+                .setRequired(true)
+                .setAutocomplete(true)
+            )
+        )
+	)
+    .listen(async(ctx) => {
+        switch (ctx.interaction.options.getSubcommand()) {
+            case "add": {
+                return ctx.interaction.showModal(addFaqModal(ctx.interaction, [], []))
+            }
+            case "remove": {
+                const faq = ctx.interaction.options.getInteger('faq', true)
+
+                const data = await ctx.database.select({
+                    title: ctx.database.schema.faqs.title,
+                    content: ctx.database.schema.faqs.content
+                }).from(ctx.database.schema.faqs)
+                    .where(eq(ctx.database.schema.faqs.id, faq))
+                    .limit(1)
+                    .then((r) => r[0])
+
+                if (!data) return ctx.interaction.reply({
+                    ephemeral: true,
+                    content: '`🔍` FAQ not found.'
+                })
+
+                await ctx.database.delete(ctx.database.schema.faqs)
+                    .where(eq(ctx.database.schema.faqs.id, faq))
+
+                return ctx.interaction.reply({
+                    ephemeral: true,
+                    content: '`✅` FAQ removed.'
+                })
+            }
+        }
+    })
+    .listenAutocomplete(async(ctx) => {
+        const search = ctx.interaction.options.getFocused(false)
+
+		const faqs = await ctx.database.select({
+			id: ctx.database.schema.faqs.id,
+			title: ctx.database.schema.faqs.title,
+		}).from(ctx.database.schema.faqs)
+			.where(ilike(ctx.database.schema.faqs.title, `%${search}%`))
+			.limit(25)
+
+		return ctx.interaction.respond(faqs.map((faq) => ({
+			name: faq.title[0] === '`' && faq.title[3] === '`' ? faq.title.slice(4) : faq.title,
+			value: faq.id
+		})))
+    })
---
+++ server/src/bot/modals/faq/add-faq.ts
@@ -0,0 +1,61 @@
+import Modal from "@/bot/modal"
+import { TextInputBuilder, TextInputStyle } from "discord.js"
+import { and, count, eq } from "drizzle-orm"
+
+export default new Modal()
+    .setName('add-faq')
+    .setTitle('Add FAQ')
+    .build((builder) => builder
+        .addRow((row) => row
+            .addComponents([
+                new TextInputBuilder()
+                    .setLabel('Title')
+                    .setCustomId('title')
+                    .setPlaceholder('title')
+                    .setStyle(TextInputStyle.Short)
+                    .setMaxLength(31)
+                    .setMinLength(2)
+                    .setRequired(true)
+            ])
+        )
+        .addRow((row) => row
+            .addComponents([
+                new TextInputBuilder()
+                    .setLabel('Content')
+                    .setCustomId('content')
+                    .setPlaceholder('content')
+                    .setStyle(TextInputStyle.Paragraph)
+                    .setMinLength(100)
+                    .setMaxLength(4096)
+                    .setRequired(true)
+            ])
+        )
+    )
+    .listen(async (ctx) => {
+        if (!ctx.interaction.guild) return
+        const title = ctx.interaction.fields.getTextInputValue('title')
+        const content = ctx.interaction.fields.getTextInputValue('content')
+        try {
+            await ctx.database.insert(ctx.database.schema.faqs)
+                .values({
+                    title,
+                    content
+                })
+
+            return ctx.interaction.reply({
+                ephemeral: true,
+                content: '`✅` FAQ has been added.'
+            })
+        } catch {
+            return ctx.interaction.reply({
+                ephemeral: true,
+                content: '`❌` This Title is already taken.',
+                embeds: [
+                    ctx.Embed()
+                        .setTitle(title)
+                        .setDescription(content)
+                ]
+            })
+        }
+    })
+    .export()
