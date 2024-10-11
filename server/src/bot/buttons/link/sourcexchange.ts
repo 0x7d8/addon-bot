0330cca608e8cdd95f3583fd8663e4ba79a7aebf
Anthony01M <anthony19990maalouf@gmail.com>
2024-10-11 11:07:25 +0000

    feat: do necessary changes

--- server/src/bot/buttons/link/sourcexchange.ts
+++ server/src/bot/buttons/link/sourcexchange.ts
@@ -3,7 +3,7 @@ import linkSourcexchangeModal from "@/bot/modals/link/sourcexchange"
 
 export default new Button()
 	.setName('link-sourcexchange')
-	.listen((ctx) => {
-		return ctx.interaction.showModal(linkSourcexchangeModal(ctx.interaction, [], []))
+	.listen(async(ctx) => {
+		return ctx.interaction.showModal(await linkSourcexchangeModal(ctx.interaction, [], []))
 	})
 	.export()
